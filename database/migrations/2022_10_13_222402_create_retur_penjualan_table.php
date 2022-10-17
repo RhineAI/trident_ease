33ee4f271ce001564f5f13075fd424453d507cbe
fadhil07 <fujinomiyan01@gmail.com>
2022-10-17 13:53:53 +0700

    Pengeditan migration yang belum sama diatur seperti penjualan, pembelian, dll

--- database/migrations/2022_10_13_222402_create_retur_penjualan_table.php
+++ database/migrations/2022_10_13_222402_create_retur_penjualan_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,9 +14,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('retur_penjualans', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('t_retur_penjualan', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->integer('id_retur_penjualan');
+            $table->integer('id_barang');
+            $table->integer('qty');
+            $table->double('harga_beli');
+            $table->double('harga_jual');
+            $table->double('sub_total');
+            $table->double('keuntungan');
+            $table->foreign('id_retur_penjualan')->references('id')->on('t_retur_penjualan')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_barang')->references('id')->on('t_barang')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
@@ -26,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('retur_penjualans');
+        Schema::dropIfExists('t_retur_penjualan');
     }
 };
