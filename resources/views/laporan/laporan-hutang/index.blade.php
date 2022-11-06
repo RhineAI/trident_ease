963b3b9592b3daa105c20d32b320862ac38a7e10
Luhung Lugina <luhung.tea@gmail.com>
2022-11-06 21:19:53 +0700

    Laporan print beres sisa Harian

--- resources/views/laporan/laporan-hutang/index.blade.php
+++ resources/views/laporan/laporan-hutang/index.blade.php
@@ -52,7 +52,7 @@ Laporan Hutang
                     </div>
                     
                     <div class="button-group mb-2">          
-                        <a href="" class="ml-2 mb-3 mt-3 btn btn-sm btn-danger text-end"><i class="fa fa-file-pdf"></i> Print PDF</a>
+                        <a href="{{ route('laporan-hutang.print', [$tanggalAwal, $tanggalAkhir] ) }}" class="ml-2 mb-3 mt-3 btn btn-sm btn-danger text-end"><i class="fa fa-file-pdf"></i> Print PDF</a>
                         <a href="{{ route('laporan-hutang.download', [$tanggalAwal, $tanggalAkhir] ) }}" class="mb-3 ml-2 mt-3 btn btn-sm btn-success text-end"><i class="fa fa-download"></i> Download PDF</a>           
                     </div>
                 {{-- <a href="{{ route('list-transaksi.export_pdf', [$tanggalAwal, $tanggalAkhir] ) }}" target="_blank" class="btn btn-danger btn-sm btn-flat" ><i class="bi bi-filetype-pdf"></i> Export PDF</a> --}}
--- resources/views/laporan/laporan-kesesuaian-stok/index.blade.php
+++ resources/views/laporan/laporan-kesesuaian-stok/index.blade.php
@@ -78,7 +78,7 @@ Laporan Kesesuaian Stok
                     </div>
                     
                     <div class="button-group mb-2">          
-                        <a href="" class="ml-2 mb-3 mt-3 btn btn-sm btn-danger text-end"><i class="fa fa-file-pdf"></i> Print PDF</a>
+                        <a href="{{ route('laporan-kesesuaian-stok.print', [$merk, $category, $tanggalAwal, $tanggalAkhir]) }}" class="ml-2 mb-3 mt-3 btn btn-sm btn-danger text-end"><i class="fa fa-file-pdf"></i> Print PDF</a>
                         <a href="{{ route('laporan-kesesuaian-stok.download', [$merk, $category, $tanggalAwal, $tanggalAkhir] ) }}" class="ml-2 mb-3 mt-3 btn btn-sm btn-success text-end"><i class="fa fa-download"></i> Download PDF</a>           
                     </div>
 
