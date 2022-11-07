85245fcfae81c479bef1527456057efe09afb077
Luhung Lugina <luhung.tea@gmail.com>
2022-11-07 17:08:16 +0700

    added helper for dashboard

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -79,3 +79,76 @@ function persentasePerbandingan($getDataYesterday, $getDataToday, $countDataNow)
     return $percentage;
 }
 
+function persentasePerbandinganSemuaData($getDataToday, $getAllData) {
+    if ($getAllData == 0) {
+        $times = 100 * $getDataToday;
+        if ($times > 1000) {
+            $percentage = 1000 . '+';
+        } else {
+            $percentage = $times;
+        }
+    } else {
+        $difference = $getAllData - $getDataToday;
+        $getPercentage = 100 / $getAllData;
+        $times = $getPercentage * $difference;
+        if ($times > 1000) {
+            $percentage = 1000 . '+';
+        } else {
+            $percentage = $times;
+        }
+    }
+    return $percentage;
+}
+
+function persentasePerbandinganHarga($getLatestTotal, $getNowTotal ) {
+    if ($getLatestTotal == 0) {
+        if($getNowTotal > 10) {
+            $percentage = $getNowTotal;
+        } elseif ($getNowTotal > 100) {
+            $percentage = $getNowTotal / 10;
+        } elseif ($getNowTotal > 1000) {
+            $percentage = $getNowTotal / 10;
+        } elseif ($getNowTotal > 10000) {
+            $percentage = $getNowTotal / 100;
+        } elseif ($getNowTotal > 100000) {
+            $percentage = $getNowTotal / 1000;
+        } elseif ($getNowTotal > 1000000) {
+            $percentage = $getNowTotal / 10000;
+        } elseif ($getNowTotal > 10000000) {
+            $percentage = $getNowTotal / 100000;
+        } elseif ($getNowTotal > 100000000) {
+            $percentage = $getNowTotal / 1000000;
+        } elseif ($getNowTotal > 1000000000) {
+            $percentage = $getNowTotal / 10000000;
+        } elseif ($getNowTotal > 10000000000) {
+            $percentage = $getNowTotal / 100000000;
+        } elseif ($getNowTotal > 100000000000) {
+            $percentage = $getNowTotal / 1000000000;
+        } elseif ($getNowTotal > 1000000000000) {
+            $percentage = $getNowTotal / 10000000000;
+        } 
+    } else {
+        $difference = $getNowTotal - $getLatestTotal;
+        // JIKA KURANG DARI    $differece = 1000 - 2000 (-1000); 
+        // JIKA LEBIH DARI     $differece = 5000 - 1000 (4000); 
+        // JIKA DATA LAST 0    $difference = 1000 - 0 (1000);
+
+        if ($difference <= $getLatestTotal) { //-1000
+            $makePositive = -1 * $difference; //1000 
+            $divide = 100 / $getLatestTotal; //100 / 2000 = 0.05
+            $percentage = round($makePositive * $divide, 2, PHP_ROUND_HALF_UP); //1000 * 0.05 = 50
+        } elseif ($difference == $getLatestTotal) {
+            $percentage = 100;
+        } elseif ($differece >= $getLatestTotal) { //4000
+            $divide = 100 / $getLatestTotal; //100 / 1000 = 0.1
+            $times = round($differece * $divide, 2, PHP_ROUND_HALF_UP); // 4000 * 0.1 = 500
+            if ($times > 1000) {
+                $percentage = 1000 . '+';
+            } else {
+                $percentage = $times;
+            }
+        }
+    }
+
+    return $percentage;
+}
