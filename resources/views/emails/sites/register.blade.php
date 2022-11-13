0f32929926ae45b58eb267b7e5edb4b744144fd3
Luhung Lugina <luhung.tea@gmail.com>
2022-11-13 21:10:20 +0700

    feat: Email register

--- resources/views/emails/sites/register.blade.php
+++ resources/views/emails/sites/register.blade.php
@@ -14,19 +14,30 @@ Username = {{ str_replace(' ', '', $user->username) }}
 <br>
 Password = 12345
 
-Pastikan username dan password default untuk segera diganti!
-<br>
+<b>Pastikan username dan password untuk segera diganti!</b>
+<br><br>
 Jika Ada Masalah<a href="https://wa.wizard.id/31a293" style="text-decoration:none; color:#3cded8;"> Hubungi Kami</a>
 
-<div class="button" style="background-color: black; padding:4px; border:1px solid black; border-radius:10%;">
-    <a href="{{ url('/') }}" style="color:white; text-align:center; text-decoration:none;">
-        Silahkan Klik Disini Untuk Melanjutkan
-    </a>
-</div>
-<br>
-@component('mail::button', ['url' => "{{ url('/') }}" ])
+<div class="button" style="  background-color: black; /* Green */
+    border: none;
+    color: white;
+    padding: 8px 11.5px;
+    text-align: center;
+    align-items:center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius:20px;
+    margin: 4px 2px;
+    cursor: pointer;">
+        <a href="{{ url('/') }}" style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding-top:20px; color:white;  text-decoration:none;">
+            Silahkan Klik Disini Untuk Melanjutkan
+        </a>
+    </div>
+<br><br>
+{{-- @component('mail::button', ['url' => "{{ url('/') }}" ])
 Silahkan Klik Disini Untuk Melanjutkan
-@endcomponent
+@endcomponent --}}
 
 Thanks,<br>
 {{ config('app.name') }}
